60ad96ad19650257693c61653c8467fc23e260ce
Javier <Javier@PC-JAVIERVILLARREAL>
2022-05-24 14:37:09 +0200

    Correccion de los elementos vistos en clase

--- src/logicaJuego/Juego.java
+++ src/logicaJuego/Juego.java
@@ -18,30 +18,34 @@ public class Juego {
 	private int dado;
 
 	/**
-	 * Constrcutor de juego en el que indicamos el tipo de personaje por parametro, después hacemos un bucle para comparar
-	 * el numero de jugadores, es decir el contador, con el limite, y lo creamos hasta que se llegue a este limite. También llamamos en el contructor al metodo
-	 * que se encarga de crear el tablero.
+	 * Constrcutor de juego en el que indicamos el tipo de personaje por parametro,
+	 * después hacemos un bucle para comparar el numero de jugadores, es decir el
+	 * contador, con el limite, y lo creamos hasta que se llegue a este limite.
+	 * También llamamos en el contructor al metodo que se encarga de crear el
+	 * tablero.
+	 * 
 	 * @param personaje
 	 */
 	public Juego(PlayerType[] personaje) {
 		super();
-		int contador=0;
+		int contador = 0;
 		coordenadaJugadores = new ArrayList<>();
 		tablero = new HashMap<>();
-		
+
 		crearTablero();
-		while(contador<Constantes.NUM_JUGADORES) {
-			if(crearJugador(personaje[contador])==true) {
+		while (contador < Constantes.NUM_JUGADORES) {
+			if (crearJugador(personaje[contador]) == true) {
 				contador++;
 			}
 		}
-		
+
 	}
-	
-	
+
 	/**
-	 * Metodo para crear a un jugador en el cual se le introduce por paramtero el tipo que es y a la hora de crearlo
-	 * compruebo que la coordenada no este ocupada, si esta no lo está, lo creo
+	 * Metodo para crear a un jugador en el cual se le introduce por paramtero el
+	 * tipo que es y a la hora de crearlo compruebo que la coordenada no este
+	 * ocupada, si esta no lo está, lo creo
+	 * 
 	 * @param tipo
 	 * @return Devuelve un tipo booleano, el cual define si se ha podido crear o no.
 	 */
@@ -51,20 +55,20 @@ public class Juego {
 		Coordenada coordenada = new Coordenada();
 		while (coordenadaJugadores.contains(coordenada)) {
 			coordenada = new Coordenada();
-		}if(this.tablero.get(coordenada)==null) {
+		}
+		if (this.tablero.get(coordenada) == null) {
 			coordenadaJugadores.add(coordenada);
 			tablero.put(coordenada, jugador);
 			crear = true;
 		}
-		
-		
+
 		return crear;
 	}
-	
-	
+
 	/**
-	 * Creo el tablero al cual lo unico que hago es llamar a los demás metodos los cuales van a asignar tanto rocas, como gemas,etc
-	 * en las coordenadas del tablero, para de esta manera tenerlo con todos los elementos creados.
+	 * Creo el tablero al cual lo unico que hago es llamar a los demás metodos los
+	 * cuales van a asignar tanto rocas, como gemas,etc en las coordenadas del
+	 * tablero, para de esta manera tenerlo con todos los elementos creados.
 	 * 
 	 */
 	private void crearTablero() {
@@ -73,12 +77,13 @@ public class Juego {
 		crearPociones();
 		crearRocas();
 	}
-	
+
 	/**
-	 * Metodo para crear roca, el numero de rocas esta limitado por la constante NUM_ROCAS, para ello 
-	 * creamos un elemento y una coordenada en la cual en el tablero comprobamos que el valor clave, es null
-	 * si este es el null, le ponemos la coordenada creada y el elemento e incrementamos el contador, hasta que 
-	 * el contador sea mayor que el limite.
+	 * Metodo para crear roca, el numero de rocas esta limitado por la constante
+	 * NUM_ROCAS, para ello creamos un elemento y una coordenada en la cual en el
+	 * tablero comprobamos que el valor clave, es null si este es el null, le
+	 * ponemos la coordenada creada y el elemento e incrementamos el contador, hasta
+	 * que el contador sea mayor que el limite.
 	 * 
 	 */
 	private void crearRocas() {
@@ -93,9 +98,11 @@ public class Juego {
 
 		}
 	}
+
 	/**
-	 * Metodo para crear dinero, hago un bucle en el cual incremento un contador, creo una coordenada y un elemento, 
-	 * y si la coordenada obtenida mediante el get es null, la añado, si no, pues aumento el contador;
+	 * Metodo para crear dinero, hago un bucle en el cual incremento un contador,
+	 * creo una coordenada y un elemento, y si la coordenada obtenida mediante el
+	 * get es null, la añado, si no, pues aumento el contador;
 	 */
 	private void crearDinero() {
 		int contador = 0;
@@ -110,27 +117,32 @@ public class Juego {
 		}
 
 	}
+
 	/**
 	 * Obtener elemento del tablero, se le pasa coordenada por parametro
+	 * 
 	 * @param coord
 	 * @return
 	 */
 	public Element obtenerElementoTablero(Coordenada coord) {
 		return this.tablero.get(coord);
 	}
+
 	/**
 	 * Obtener coordenada del jugador
+	 * 
 	 * @return
 	 */
 	public Coordenada obtenerCoordenadaJugadorJuega() {
 		return this.coordenadaJugadores.get(jugadorJuega);
 	}
-	
+
 	/**
-	 * Metodo para crear gemas, el numero de rocas esta limitado por la constante NUM_GEMAS, para ello 
-	 * creamos un elemento y una coordenada en la cual en el tablero comprobamos que el valor clave, es null
-	 * si este es el null, le ponemos la coordenada creada y el elemento e incrementamos el contador, hasta que 
-	 * el contador sea mayor que el limite.
+	 * Metodo para crear gemas, el numero de rocas esta limitado por la constante
+	 * NUM_GEMAS, para ello creamos un elemento y una coordenada en la cual en el
+	 * tablero comprobamos que el valor clave, es null si este es el null, le
+	 * ponemos la coordenada creada y el elemento e incrementamos el contador, hasta
+	 * que el contador sea mayor que el limite.
 	 * 
 	 */
 	private void crearGemas() {
@@ -145,11 +157,13 @@ public class Juego {
 
 		}
 	}
+
 	/**
-	 * Metodo para crear pociones, el numero de rocas esta limitado por la constante NUM_POCIONES, para ello 
-	 * creamos un elemento y una coordenada en la cual en el tablero comprobamos que el valor clave, es null
-	 * si este es el null, le ponemos la coordenada creada y el elemento e incrementamos el contador, hasta que 
-	 * el contador sea mayor que el limite.
+	 * Metodo para crear pociones, el numero de rocas esta limitado por la constante
+	 * NUM_POCIONES, para ello creamos un elemento y una coordenada en la cual en el
+	 * tablero comprobamos que el valor clave, es null si este es el null, le
+	 * ponemos la coordenada creada y el elemento e incrementamos el contador, hasta
+	 * que el contador sea mayor que el limite.
 	 * 
 	 */
 	private void crearPociones() {
@@ -164,14 +178,16 @@ public class Juego {
 
 		}
 	}
-	
-/**
- * Metodo para terminar el juego, el cual recorre los elementos del tablero, y comprueba si el elemento es una estancia de jugador
- * y si lo es comprobamos si ha llegado al limite de dinero del juego el cual es 4, si es así acaba y ponemos la variable
- * a true. Si no lo posee pero la longitud de la coordenada es igual a 1, también acabariamos el juego, por lo cual lo pondriamos
- * a true.
- * @return terminar, variable booleana.
- */
+
+	/**
+	 * Metodo para terminar el juego, el cual recorre los elementos del tablero, y
+	 * comprueba si el elemento es una estancia de jugador y si lo es comprobamos si
+	 * ha llegado al limite de dinero del juego el cual es 4, si es así acaba y
+	 * ponemos la variable a true. Si no lo posee pero la longitud de la coordenada
+	 * es igual a 1, también acabariamos el juego, por lo cual lo pondriamos a true.
+	 * 
+	 * @return terminar, variable booleana.
+	 */
 	public boolean isTerminado() {
 		boolean terminar = false;
 		boolean dinero = false;
@@ -182,13 +198,15 @@ public class Juego {
 				}
 			}
 		}
-	if (this.coordenadaJugadores.size() == 1 || dinero) {
-		terminar = true;
+		if (this.coordenadaJugadores.size() == 1 || dinero) {
+			terminar = true;
 		}
 		return terminar;
 	}
+
 	/**
 	 * Metodo para eliminar a un jugador
+	 * 
 	 * @param coordenada
 	 */
 	private void eliminarJugador(Coordenada coordenada) {
@@ -197,104 +215,112 @@ public class Juego {
 	}
 
 	/**
-	 * Metodo para imprimir jugadores, el cual declaramos un StringBuilder que va a ser lo que retorne el método,
-	 * recorremos las coordenadas que posee la Lista, despues creamos u  jugador  y le realizamos el casteo para poder
-	 * obtener su nombre y metemos en el StringBuilder su nombre y el numero de jugador.
+	 * Metodo para imprimir jugadores, el cual declaramos un StringBuilder que va a
+	 * ser lo que retorne el método, recorremos las coordenadas que posee la Lista,
+	 * despues creamos u jugador y le realizamos el casteo para poder obtener su
+	 * nombre y metemos en el StringBuilder su nombre y el numero de jugador.
+	 * 
 	 * @return Devuleve el StringBuilder.
 	 */
 	public String imprimeNombreJugadores() {
 		int contador = 1;
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (Coordenada coordenada : this.coordenadaJugadores) {
-		Jugador jugadores = (Jugador) tablero.get(coordenada);
-			sb.append("El tipo del jugador es: " + jugadores.getNombre() + " y es el jugador numero: " + contador + "\n");
+			Jugador jugadores = (Jugador) tablero.get(coordenada);
+			sb.append(
+					"El tipo del jugador es: " + jugadores.getNombre() + " y es el jugador numero: " + contador + "\n");
 			contador++;
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 * Obtener valor del dado
+	 * 
 	 * @return
 	 */
 	public int getValorDado() {
 		return dado;
 	}
+
 	/**
 	 * Disminuye valor del dado
+	 * 
 	 * @return
 	 */
 	public int decrementaDado() {
 		return this.dado--;
 	}
-	
+
 	/**
 	 * Metodo para asignar valor del dado
 	 */
 	public void setDado() {
 		this.dado = ((Jugador) tablero.get(coordenadaJugadores.get(jugadorJuega))).getVelocidadParaLuchar();
 	}
-	
-	
+
 	/**
-	 * Metodo el cual cambia al jugador de posicion, se le pasa por parametro una coordenada, acto seguido se elimina al 
-	 * juagdor de la coordenada, y se actualiza cambiandolo de posicion, y se le vuelve a poner en el tablero.
+	 * Metodo el cual cambia al jugador de posicion, se le pasa por parametro una
+	 * coordenada, acto seguido se elimina al juagdor de la coordenada, y se
+	 * actualiza cambiandolo de posicion, y se le vuelve a poner en el tablero.
+	 * 
 	 * @param coordenada
 	 */
 	private void cambiaJugadorAPosicion(Coordenada coordenada) {
-		Coordenada coor = coordenadaJugadores.get(jugadorJuega); 
+		Coordenada coor = coordenadaJugadores.get(jugadorJuega);
 		Jugador jug = (Jugador) tablero.get(coor);
-		
-		tablero.remove(coor); 
-		coordenadaJugadores.remove(jugadorJuega); 
-		coordenadaJugadores.add(jugadorJuega, coordenada); 
+
+		tablero.remove(coor);
+		coordenadaJugadores.remove(jugadorJuega);
+		coordenadaJugadores.add(jugadorJuega, coordenada);
 		tablero.put(coordenada, jug);
 	}
-	
 
 	/**
-	 * Metodo el cual imprime el valor de los elementos del jugadores, se realiza igual que el anterior solo que a este
-	 * le sacamos los valores de los elementos del jugador.
+	 * Metodo el cual imprime el valor de los elementos del jugadores, se realiza
+	 * igual que el anterior solo que a este le sacamos los valores de los elementos
+	 * del jugador.
+	 * 
 	 * @return Devuelve un StringBuilder
 	 */
 	public String imprimeValoresJugadores() {
 		int contador = 1;
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (Coordenada coordenada : this.coordenadaJugadores) {
 			Jugador jugador = (Jugador) tablero.get(coordenada);
-			sb.append("Jugador numero: " + contador 
-						+ " Dinero: " + jugador.getDinero() 
-						+ " Gemas: " + jugador.getGemas() 
-						+ " Pociones: " + jugador.getPociones() + "\n");
+			sb.append("Jugador numero: " + contador + " Dinero: " + jugador.getDinero() + " Gemas: "
+					+ jugador.getGemas() + " Pociones: " + jugador.getPociones() + "\n");
 			contador++;
 		}
 		return sb.toString();
 	}
-	
+
 	/**
-	 * Metodo para mover al jugador, se le pasa por parametro un caracter, en funcion de la direccion en la cual quiere ir,
-	 * una vez que se le pasa se llama a los metodos correspondientes para la subida, bajada o desplazamiento hacia
+	 * Metodo para mover al jugador, se le pasa por parametro un caracter, en
+	 * funcion de la direccion en la cual quiere ir, una vez que se le pasa se llama
+	 * a los metodos correspondientes para la subida, bajada o desplazamiento hacia
 	 * las direcciones correctas, si no, se lanza excepcion.
+	 * 
 	 * @param direccion, Caracter
 	 * @return
 	 * @throws JuegoException
 	 */
-	private Coordenada getNextPosition (char direccion) throws JuegoException {
-		if(direccion!='N' && direccion!='S' && direccion!='E' && direccion!='O') {
+	private Coordenada getNextPosition(char direccion) throws JuegoException {
+		if (direccion != 'N' && direccion != 'S' && direccion != 'E' && direccion != 'O') {
 			throw new JuegoException("Error en la dirección.");
 		}
 		Coordenada coor;
 		try {
 			coor = coordenadaJugadores.get(jugadorJuega).clone();
-			if(direccion=='N') {
+			if (direccion == 'N') {
 				coor.goUp();
-			}else if(direccion=='S') {
+			} else if (direccion == 'S') {
 				coor.goDown();
-			}else if(direccion=='E') {
+			} else if (direccion == 'E') {
 				coor.goRight();
-			}else {
+			} else {
 				coor.goLeft();
 			}
 		} catch (CloneNotSupportedException e) {
@@ -302,7 +328,7 @@ public class Juego {
 		}
 		return coor;
 	}
-	
+
 	/**
 	 * Escribe el tablero en formato no gráfico. Devuelve el string con la
 	 * información
@@ -332,23 +358,24 @@ public class Juego {
 		resul.delete(resul.length() - 5, resul.length());
 		return resul.toString();
 	}
-	
+
 	public void proximoJugador() {
-		if (this.jugadorJuega == Constantes.NUM_JUGADORES - 1) {
+		if (this.jugadorJuega == coordenadaJugadores.size() - 1) {
 			this.jugadorJuega = 0;
 		} else {
 			jugadorJuega++;
 		}
 	}
-	
+
 	/**
 	 * Metodo para obtener ganador
+	 * 
 	 * @return
 	 */
 	public String getGanador() {
 		StringBuilder resultado = new StringBuilder();
 		if (this.coordenadaJugadores.size() == 1) {
-			Jugador aux = (Jugador) tablero.get(coordenadaJugadores.get(jugadorJuega));
+			Jugador aux = (Jugador) tablero.get(coordenadaJugadores.get(0));
 			resultado.append(aux.toString());
 		} else {
 			for (Element siguiente : tablero.values()) {
@@ -365,20 +392,21 @@ public class Juego {
 
 	/**
 	 * Metodo para obtener el nombre del jugador
+	 * 
 	 * @return
 	 */
 	public String getNombreJugadorQueJuega() {
 		StringBuilder sb = new StringBuilder();
 		Coordenada coor = this.coordenadaJugadores.get(jugadorJuega);
-		
-	
+
 		Jugador jugador = (Jugador) this.tablero.get(coor);
 		sb.append(jugador.getNombre());
 		return sb.toString();
 	}
-	
+
 	/**
 	 * Obtener movimiento del jugador
+	 * 
 	 * @return
 	 */
 	public int getMovimientoJugador() {
@@ -386,7 +414,6 @@ public class Juego {
 		Jugador jugador = (Jugador) this.tablero.get(coor);
 		return jugador.getFuerzaParaLuchar();
 	}
-	
 
 	/**
 	 * Simplemente escribe una barra en pantalla
@@ -402,7 +429,6 @@ public class Juego {
 		resul.append("\n");
 		return resul.toString();
 	}
-
 
 	/**
 	 * Mover el jugador
@@ -498,5 +524,4 @@ public class Juego {
 		return resul;
 	}
 
-	
 }
